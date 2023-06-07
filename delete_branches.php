e8926a86d9684661489c174d0f7504a36c7c9802
SurenAvagyanG <suren@fifth-llc.com>
2023-06-07 19:23:20 +0400

    asdasd

--- delete_branches.php
+++ delete_branches.php
@@ -1,6 +1,6 @@
 <?php
-$branchesToPreserve  = ['development', 'staging', 'pre-production', 'production', 'master']; // Replace with the names of the branches to exclude
-$branchNamePrefix = 'feat/'; // Replace with the desired branch name prefix
+$branchesToPreserve = ['development', 'staging', 'pre-production', 'production', 'master', 'feat/not-delete-1', 'fix/not-delete-1']; // Replace with the names of the branches to exclude
+$branchNamePrefixes = ['feat/', 'fix/']; // Replace with the desired branch name prefix
 $remoteName = 'origin';
 
 // Execute the Git command to retrieve the remote branch list
@@ -12,24 +12,23 @@ $branches = explode("\n", $output);
 $branches = array_map('trim', $branches);
 $branches = array_filter($branches);
 
-//  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
-
 // Iterate over the remote branches and delete the ones that match the criteria
 foreach ($branches as $branch) {
     // Extract the branch name from the output
     $branchName = explode('refs/heads/', $branch)[1];
+    foreach ($branchNamePrefixes as $prefix) {
+        // Check if the branch starts with the desired prefix
+        if (strpos($branchName, $prefix) === 0 && !in_array($branchName, $branchesToPreserve)) {
+            // Execute the Git command to delete the remote branch
+            $command = "git push $remoteName --delete $branchName";
+            $output = shell_exec($command);
 
-    // Check if the branch starts with the desired prefix
-    if (strpos($branchName, $branchNamePrefix) === 0 && !in_array($branchName, $branchesToPreserve)) {
-        // Execute the Git command to delete the remote branch
-        $command = "git push $remoteName --delete $branchName";
-        $output = shell_exec($command);
-
-        // Check the output for any error messages
-        if (strpos($output, 'error:') !== false) {
-            echo "Error: Failed to delete the remote branch '$branchName'.";
-        } else {
-            echo "Remote branch '$branchName' has been deleted successfully.";
+            // Check the output for any error messages
+            if (strpos($output, 'error:') !== false) {
+                echo "Error: Failed to delete the remote branch '$branchName'.";
+            } else {
+                echo "Remote branch '$branchName' has been deleted successfully.";
+            }
         }
     }
 }
